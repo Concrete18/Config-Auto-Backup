89d03233019dc273a1325d1c4fafea406392ad6c
Concrete18 <michaelericson19@gmail.com>
2020-09-23 22:45:14 -0400

    Fixed copy bug.

--- Backup.py
+++ Backup.py
@@ -15,7 +15,7 @@ class Backup_App:
 
 
     def Delete_Oldest(self):
-        '''Deletest the oldest backups so only the the newest specified in backup_redundancy is left.'''
+        '''Deletest the oldest backups so only the newest specified in backup_redundancy is left.'''
         backup_list = []
         dir = self.backup_location
         for file in os.listdir(dir):
@@ -25,7 +25,7 @@ class Backup_App:
             print(f'{self.backup_redundancy} or Less Backups.')
             return
         else:
-            print(f'More then {self.backup_redundancy} backups.\nDeleting oldest now.')
+            print(f'More than {self.backup_redundancy} backups.\nDeleting oldest now.')
             sorted_list = sorted(backup_list, key=os.path.getctime, reverse=True)
             for i in range(self.backup_redundancy, len(backup_list)):
                 shutil.rmtree(sorted_list[i])
@@ -35,29 +35,27 @@ class Backup_App:
         '''Backups up the entered folder.'''
         current_time = dt.datetime.now().strftime("%d-%m-%y %H-%M")
         dest = os.path.join(self.backup_location, current_time)
+        os.mkdir(dest)
         for item, loc in self.backup_targets.items():
-            final_dest = os.path.join(dest, item)
-            # try:
+            base_folder = os.path.join(dest, item)
+            final_dest = os.path.join(base_folder, os.path.basename(loc))
+            if not os.path.isdir(final_dest):
+                os.mkdir(base_folder)
             if os.path.isfile(loc):
-                print(loc)
                 shutil.copy(loc, final_dest)
             else:
                 shutil.copytree(loc, final_dest)
             print(f'Backed Up: {item} from {loc}')
-            # except FileNotFoundError:
-            #     print('No Action Completed - File location does not exist.')
-            # except FileExistsError:
-            #     print('No Action Completed - Save Already Backed up.')
             print()
 
 
     def Restore(self):
-        '''Asks what you want to restore then restores it to is set location.'''
+        '''Asks what you want to restore then restores it to its set location.'''
         pass
 
 
 if __name__ == '__main__':
     Configs = Backup_App()
-    # input('Would you like to backup your files?')
+    # input('Would you like to back up your files?')
     Configs.Backup()
     Configs.Delete_Oldest()
