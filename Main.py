17c9e0411cd817525bec74d8b17d6eba599b68eb
Concrete18 <michaelericson19@gmail.com>
2020-09-24 15:34:10 -0400

    Added Main.py

---
+++ Main.py
@@ -0,0 +1,13 @@
+from Backup import Backup_App
+
+Configs = Backup_App()
+
+response = input('Multi Backup System\n\nWhat would you like to do?\n1. Backup\n2. Restore\n')
+if response == '1':
+    print('Fuck')
+    Configs.Backup()
+    Configs.Delete_Oldest()
+elif response == '2':
+    Configs.Restore()
+else:
+    print('Unknown Response')
