d5f49b1938d8324fcd8025ec0c453e3b6c0ba23d
Concrete18 <michaelericson19@gmail.com>
2020-09-24 15:34:41 -0400

    Added Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ Configs = Backup_App()
 
 response = input('Multi Backup System\n\nWhat would you like to do?\n1. Backup\n2. Restore\n')
 if response == '1':
-    print('Fuck')
     Configs.Backup()
     Configs.Delete_Oldest()
 elif response == '2':
