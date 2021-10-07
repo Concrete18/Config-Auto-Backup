ef6c3c5c39ec509a28201115d29127040654cf8a
Concrete18 <michaelericson19@gmail.com>
2021-10-07 00:09:06 -0400

    working on refactor

--- Main.py
+++ Main.py
@@ -1,14 +1,32 @@
-from Backup import Backup_App
+from classes.Backup_Restore import Backup_Restore
 
-Configs = Backup_App()
+class Main:
 
-response = input('Multi Backup System\n\nWhat would you like to do?\n1. Backup\n2. Restore\n')
-print()
-if response == '1':
-    Configs.Backup()
-    Configs.Delete_Oldest()
-elif response == '2':
-    Configs.Restore()
-else:
-    print('Unknown Response')
-input()
+    App = Backup_Restore()
+
+
+    def run(self):
+        '''
+        ph
+        '''
+        try:
+            print('Multi Backup System')
+            # prints missing backup targets
+            print('\nChecking for missing target paths.')
+            missing_entries = ", ".join(self.App.check())
+            print(missing_entries)
+            response = input('\n\nWhat would you like to do?\n1. Backup\n2. Restore\n')
+            if response == '1':
+                self.App.Backup()
+                self.App.Delete_Oldest()
+            elif response == '2':
+                self.App.Restore()
+            else:
+                print('Unknown Response')
+                input()
+        except KeyboardInterrupt:
+            exit()
+
+
+if __name__ == '__main__':
+    Main().run()
